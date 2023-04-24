5995538d79e8c6a858fa698b7922f7cf44c3e487
Xavian Moody <XavianMoody1025@gmail.com>
2023-04-24 10:55:12 -0400

    File cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,5 @@
-import { useState } from "react";
-import { Header } from "./features/Header/Header";
-import { About } from "./sections/About/About";
-import { Contact } from "./sections/Contact/Contact";
-import { Hero } from "./sections/Hero/Hero";
-import { Projects } from "./sections/Projects/Projects";
-
 function App() {
-  const [section, setSection] = useState([0, 0]);
-
-  return (
-    <div className="App">
-      <Header setSection={setSection} section={section}></Header>
-      <Hero setSection={setSection}></Hero>
-      <About setSection={setSection}></About>
-      <Projects setSection={setSection}></Projects>
-      <Contact setSection={setSection}></Contact>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
